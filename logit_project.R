7cf7543a3f9389c592b4dff63082fe34823fa874
Michal <michal.thor5@gmail.com>
2019-05-19 12:54:33 +0200

    Data cleared

--- logit_project.R
+++ logit_project.R
@@ -11,7 +11,22 @@ if(!require(caret)){
   install.packages("caret")
   library(caret)
 }
-
+if(!require(corrplot)){
+  install.packages("corrplot")
+  library(corrplot)
+}
+if(!require(tibble)){
+  install.packages("tibble")
+  library(tibble)
+}
+if(!require(tidyr)){
+  install.packages("tidyr")
+  library(tidyr)
+}
+if(!require(Rpdb)){
+  install.packages("Rpdb")
+  library(Rpdb)
+}
 
 # Loading dataset
 df <- read.arff("data/1year.arff")
@@ -22,52 +37,64 @@ df %>%
   filter(class == 1) %>% 
   nrow()
 
-# Getting all the columns with a lot of NA's
-columns <- df %>%
+
+# Checking how many NA's there are for each variable
+df %>% 
+  summarise_all(funs(sum(is.na(.)))) %>% 
+  Filter(function(x) { x > 0},.)
+
+
+# Checking how many rows are with NA's
+df %>% 
+  rowwise() %>% 
+  is.na() %>% 
+  rowSums() %>% 
+  Filter(function(x) x > 0,.) %>% 
+  length()
+
+# Setting factor variables for missing data 
+# (65 - no long-term liabilities, 66 - no financial expenses, 67 - no inventory)
+df$Attr65 <- as.factor(apply(df, 1, FUN = function(x) if (is.na(x['Attr37'])) 1 else 0))
+df$Attr66 <- as.factor(apply(df, 1, FUN = function(x) if (is.na(x['Attr27'])) 1 else 0))
+df$Attr67 <- as.factor(apply(df, 1, FUN = function(x) if (is.na(x['Attr60'])) 1 else 0))
+
+# Getting all the columns with a lot of NA's (over 1% of observations are NA)
+na_columns <- df %>%
   select(everything()) %>%
   summarise_all(funs(sum(is.na(.)))) %>%
   Filter(function(x) { x > 0.01*nrow(df)},.) %>% 
   colnames()
 
 
-# Getting all the rows with a lot of NA's
-rows <- df %>% 
-  rowwise() %>% 
-  is.na() %>% 
-  rowSums() %>% 
-  Filter(function(x) x > 0.05*ncol(df),.) %>% 
-  list() %>% 
-  do.call(rbind, .) %>% 
-  colnames() %>% 
-  as.vector()
-
 # Subsetting dataframe without highly not available data
-df1 <- df[ , -which(names(df) %in% columns)]
+df1 <- df[ , -which(names(df) %in% na_columns)]
 
-rows1 <- df1 %>% 
+# Getting all the rows with over 2 NA values (including 6 defaults - 
+# they make for ~6% of deleted rows so the proportion is remained considering whole dataset for which default ratio is ~4%)
+na_rows <- df1 %>% 
   rowwise() %>% 
   is.na() %>% 
   rowSums() %>% 
-  Filter(function(x) x > 0.05*ncol(df1),.) %>% 
+  Filter(function(x) x > 2,.) %>% 
   list() %>% 
   do.call(rbind, .) %>% 
   colnames() %>% 
   as.vector()
 
-df2 <- df1[-which(rownames(df1) %in% rows1),]
-#---------------- Lost 93 instances (including 6 defaults)
+# Omitting rows containing a lot of NA's
+df2 <- df1[-which(rownames(df1) %in% na_rows),]
 
 
 # Creating correlation matrix
-df2[,-which(names(df2)=="class")] %>%
+df2[,-which(names(df2) %in% c("class", "Attr65", "Attr66", "Attr67"))] %>%
   na.omit() %>% 
   as.matrix() %>% 
   cor() %>%  
   corrplot(., method="circle",type = "upper")
 
 
-# Choose variables which are correlatd with over 6 others with corr > 0.95
-corr_vars <- df2[,-which(names(df2)=="class")] %>%
+# Choosing variables which are correlated with over 6 others with corr > 0.95
+corr_columns <- df2[,-which(names(df2) %in% c("class", "Attr65", "Attr66", "Attr67"))] %>%
   na.omit() %>% 
   as.matrix() %>% 
   cor() %>% 
@@ -79,13 +106,29 @@ corr_vars <- df2[,-which(names(df2)=="class")] %>%
   filter(.,freq >= 6) %>% 
   .[,1]
 
-df3 <- df2[,-which(names(df2) %in% corr_vars)]
+# Omitting highly correlated variables
+df3 <- df2[,-which(names(df2) %in% corr_columns)]
 
-df3 %>% 
+
+# Omitting variables 32 and 46 because they have a lot of NA's and they are highly correlated to 52 and 40 respectively
+df4 <- df3[,-which(names(df3) %in% c("Attr32","Attr46"))]
+
+
+# Omitting 4 observations, because there were NA's which were impossible to fill - not connected with any other variable in any way
+# and there were no defaults omitted.
+df4 <- df4[-which(is.na(df4["Attr61"]) | is.na(df4["Attr5"])),]
+
+
+# Resetting index
+rownames(df4) <- NULL
+
+
+# Checking NA's in dataset - where are the remaining ones occuring
+df4 %>% 
   summarise_all(funs(sum(is.na(.)))) %>% 
   Filter(function(x) { x > 0},.)
-  
-df3 %>% 
+
+df4 %>% 
   rowwise() %>% 
   is.na() %>% 
   rowSums() %>% 
@@ -93,15 +136,32 @@ df3 %>%
   length()
 
 
-
-
+# ------------------------------ NO NA's REMAINING, HURRAY!
 
 
 
 # Splitting dataset into training and testing datasets
-which_train <- createDataPartition(df2$class, 
+which_train <- createDataPartition(df4$class, 
                                    p = 0.7, 
                                    list = FALSE) 
-year1_train <- df2[which_train,]
-year1_test <- df2[-which_train,]
+year1_train <- df4[which_train,]
+year1_test <- df4[-which_train,]
 
+
+year1_logit1 <- glm(class ~ .,
+                       # here we define type of the model
+                       family =  binomial(link = "logit"),
+                       data = year1_train,
+                       maxit = 100)
+
+summary(year1_logit1)
+
+year1_predicted <- predict(year1_logit1,
+                           type = "response")
+
+head(year1_predicted)
+
+( ctable <- confusionMatrix(data = as.factor(ifelse(year1_predicted > 0.9, "0", "1")), 
+reference = year1_train$class, 
+positive = "1") 
+)
